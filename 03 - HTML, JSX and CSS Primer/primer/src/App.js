f768a6121841720b509fade3ac4f3d791a16b53d
Max <maxdeveloper16@gmail.com>
2020-12-20 22:12:22 -0500

    Add button to add count and handle events

--- 03 - HTML, JSX and CSS Primer/primer/src/App.js
+++ 03 - HTML, JSX and CSS Primer/primer/src/App.js
@@ -13,12 +13,18 @@ export default class App extends Component {
 
   getClassName(val) {
     return val % 2 === 0
-    ? "bg-primary text-white text-center p-2 m-1"
-    : "bg-secondary text-white text-center p-2 m-1"
-    }
-  
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+  }
+
+  handleClick = () => this.setState({ count: this.state.count + 1 });
+
   render = () =>
     <h4 className={this.getClassName(this.state.count)}>
+      <button className="btn btn-info m-2" onClick={this.handleClick}>
+        Click Me
+      </button>
+
       Number of things: {this.isEven(this.state.count)}
     </h4>
 }
